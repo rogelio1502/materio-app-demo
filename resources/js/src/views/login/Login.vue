d183685b7b2c74b26ba6ed8041be1bd81fc012e5
rogelio1502 <rogelio15021515@gmail.com>
2023-01-01 20:54:35 -0600

    Initial Commit, added auth paths and login form

---
+++ resources/js/src/views/login/Login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="auth-wrapper auth-v1">
+    <div class="auth-inner">
+      <v-col
+        cols="12"
+        md="12"
+      >
+        <v-card>
+          <v-card-title>Welcome to my demo!</v-card-title>
+          <v-card-text>
+            <v-form>
+              <!-- <v-text-field
+            v-model="firstname"
+            :prepend-inner-icon="icons.mdiAccountOutline"
+            label="First Name"
+            outlined
+            dense
+            placeholder="First Name"
+          ></v-text-field>
+          <v-text-field
+            v-model="lastname"
+            :prepend-inner-icon="icons.mdiAccountOutline"
+            label="Last Name"
+            outlined
+            dense
+            placeholder="Last Name"
+          ></v-text-field> -->
+              <v-text-field
+                v-model="email"
+                :prepend-inner-icon="icons.mdiEmailOutline"
+                label="Email"
+                outlined
+                dense
+                placeholder="Email"
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :prepend-inner-icon="icons.mdiLockOutline"
+                label="Password"
+                outlined
+                dense
+                type="password"
+                placeholder="password"
+              ></v-text-field>
+              <v-btn
+                block
+                color="primary"
+              > Login </v-btn>
+
+            </v-form>
+          </v-card-text>
+        </v-card>
+
+      </v-col>
+    </div>
+    <!-- tree -->
+    <v-img
+      class="auth-tree"
+      width="247"
+      height="185"
+      :src="require('@/assets/images/misc/tree.png').default"
+    ></v-img>
+
+    <!-- tree  -->
+    <v-img
+      class="auth-tree-3"
+      width="377"
+      height="289"
+      :src="require('@/assets/images/misc/tree-3.png').default"
+    ></v-img>
+  </div>
+</template>
+
+<script>
+import { mdiAccountOutline, mdiEmailOutline, mdiCellphone, mdiLockOutline } from '@mdi/js'
+import { ref } from '@vue/composition-api'
+
+export default {
+  setup() {
+    const firstname = ref('')
+    const lastname = ref('')
+    const email = ref('')
+    const password = ref()
+
+    return {
+      firstname,
+      lastname,
+      email,
+      password,
+      // icons
+      icons: {
+        mdiAccountOutline,
+        mdiEmailOutline,
+        mdiCellphone,
+        mdiLockOutline,
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import '~@resources/sass/preset/pages/auth.scss';
+</style>
---
+++ resources/js/src/views/pages/DemoDatatable.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-uppercase">
+            Id
+          </th>
+          <th class="text-center text-uppercase">
+            Titulo de la tarea
+          </th>
+          <th class="text-center text-uppercase">
+            Usuario
+          </th>
+          <th class="text-center text-uppercase">
+            Completada
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="todo in todos"
+          :key="todo.id"
+        >
+          <td>{{ todo.id }}</td>
+          <td class="text-center">
+            {{ todo.title }}
+          </td>
+          <td class="text-center">
+            {{ todo.userId }}
+          </td>
+          <td class="text-center">
+            {{ todo.completed }}
+          </td>
+
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  mounted() {
+    this.getTodos()
+  },
+  setup() {},
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  methods: {
+    getTodos() {
+      this.todos = []
+      axios.get('https://jsonplaceholder.typicode.com/todos').then(r => {
+        // console.log(r.data)
+        r.data.forEach(todo => {
+          // console.log(todo.id)
+          this.todos.push(todo)
+        })
+      })
+    },
+  },
+}
+</script>
